a03b2f5286c931f21ae4868bb5f5c28c2cc6b2b2
Paz <pietroazzari@gmail.com>
2023-03-16 14:44:28 +0100

    Add files via upload
    initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+var num1, num2, operator, answer;
+
+function generateQuestion() {
+	num1 = Math.floor(Math.random() * 10) + 1;
+	num2 = Math.floor(Math.random() * 10) + 1;
+	operator = ['+', '-', 'x'][Math.floor(Math.random() * 3)];
+	document.getElementById('num1').innerHTML = num1;
+	document.getElementById('num2').innerHTML = num2;
+	document.getElementById('operator').innerHTML = operator;
+	document.getElementById('answer').value = '';
+	document.getElementById('result').innerHTML = '';
+}
+
+function checkAnswer() {
+	var correctAnswer;
+	switch (operator) {
+		case '+':
+			correctAnswer = num1 + num2;
+			break;
+		case '-':
+			correctAnswer = num1 - num2;
+			break;
+		case 'x':
+			correctAnswer = num1 * num2;
+			break;
+	}
+	answer = parseInt(document.getElementById('answer').value);
+	if (answer === correctAnswer) {
+		document.getElementById('result').innerHTML = 'Corretto!';
+	} else {
+		document.getElementById('result').innerHTML = 'Sbagliato. La risposta corretta è ' + correctAnswer;
+	}
+}
